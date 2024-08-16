88814dee2b1956af97fbe6c15c406241045bd75b
HelmmDev <44981852+HelmmDev@users.noreply.github.com>
2024-08-16 10:46:14 -0400

    Update themes.sh

--- bin/themes.sh
+++ bin/themes.sh
@@ -7,6 +7,3 @@ for theme in "${themes[@]}"
 do
 	cp -Rf "node_modules/$theme" content/themes
 done
-
-# Ensure the genki theme is available in the content/themes directory
-cp -Rf "content/themes/genki" content/themes/genki
